c14f2fe12e3d1a3197b65b52e4b13314b4f90fc5
Ekat7 <ea28@illinois.edu>
2025-04-01 19:34:21 -0500

    added sensory

---
+++ screens/SensoryScreen.js
@@ -0,0 +1,162 @@
+import React, {useState} from 'react';
+import { StatusBar } from 'expo-status-bar';
+import {ScrollView} from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, FlatList, TouchableOpacity, TouchableHighlight, Button} from 'react-native';
+
+export default function SensoryScreen() {
+
+  const pathName = '../Logo.png';
+  const act1 = '../assets/Sensory/imageS.png';
+  const act2 = '../assets/Sensory/peanutball.png';
+  const act3 = '../assets/Sensory/PlayDoh.png';
+  const act4 = '../assets/Sensory/putty.png';
+  const act5 = '../assets/Sensory/sandpit.png';
+  const act6 = '../assets/Sensory/swing.png';
+  
+
+  const [selectedActivities, setSelectedActivities] = useState([]);
+
+  const toggleSelection = (id) => {
+    setSelectedActivities((prev) =>
+      prev.includes(id) ? prev.filter((item) => item !== id) : [...prev, id]
+    );
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image source = {require(pathName)} />
+      <ScrollView>
+        <View style = {styles.grid}>
+          <TouchableOpacity activeOpacity={0.6} onPress={() => toggleSelection(act1)}>
+            <View style={[styles.circle1, selectedActivities.includes(act1) && styles.selectedCircle]}>
+              <Image source={require(act1)} style={styles.circleImage} />
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.6} onPress={() => toggleSelection(act2)}>
+            <View style={[styles.circle2, selectedActivities.includes(act2) && styles.selectedCircle]}>
+              <Image source={require(act2)} style={styles.circleImage} />
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.6} onPress={() => toggleSelection(act3)}>
+            <View style={[styles.circle3, selectedActivities.includes(act3) && styles.selectedCircle]}>
+              <Image source={require(act3)} style={styles.circleImage} />
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.6} onPress={() => toggleSelection(act4)}>
+            <View style={[styles.circle4, selectedActivities.includes(act4) && styles.selectedCircle]}>
+              <Image source={require(act4)} style={styles.circleImage} />
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.6} onPress={() => toggleSelection(act5)}>
+            <View style={[styles.circle5, selectedActivities.includes(act5) && styles.selectedCircle]}>
+              <Image source={require(act5)} style={styles.circleImage} />
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.6} onPress={() => toggleSelection(act6)}>
+            <View style={[styles.circle6, selectedActivities.includes(act6) && styles.selectedCircle]}>
+              <Image source={require(act6)} style={styles.circleImage} />
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'top',
+    width: '100%',
+  },
+
+  grid: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-evenly',
+    width: '300px',
+  },
+
+  circle1: {
+    width: 100,
+    height: 100,
+    padding: 20,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 20,
+    marginVertical: 20,
+    backgroundColor: 'rgb(195, 229, 236)',
+  },
+
+  circleImage: {
+    width: 80,
+    height: 80,
+    resizeMode: 'contain',
+  },
+
+  circle2: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 20,
+    marginVertical: 20,
+    backgroundColor: 'rgb(195, 229, 236)',
+  },
+
+  circle3: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 20,
+    marginVertical: 20,
+    backgroundColor: 'rgb(195, 229, 236)',
+  },
+
+  circle4: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 20,
+    marginVertical: 20,
+    backgroundColor: 'rgb(195, 229, 236)',
+  },
+
+  circle5: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 20,
+    marginVertical: 20,
+    backgroundColor: 'rgb(195, 229, 236)',
+  },
+
+  circle6: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 20,
+    marginVertical: 20,
+    backgroundColor: 'rgb(195, 229, 236)',
+  },
+
+  selectedCircle: {
+    borderWidth: 3,
+    backgroundColor: 'rgb(211,211,211)',
+    borderWidth: 0,
+  },
+
+});
